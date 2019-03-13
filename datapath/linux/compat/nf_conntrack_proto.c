ca3022da75216cbfa7587dfc4b590f09fa46bd5d
Yifeng Sun <pkusunyifeng@gmail.com>
2019-03-13 23:08:41 +0000

    Revert "temp"
    This reverts commit 2923201fc1f803187d37e7fc0f8602c26052ad23.

--- datapath/linux/compat/nf_conntrack_proto.c
+++ datapath/linux/compat/nf_conntrack_proto.c
@@ -1,10 +1,7 @@
 #include <linux/types.h>
 
 #include <net/netfilter/nf_conntrack.h>
-
-#ifndef HAVE_IPV6_FRAG_H
 #include <net/netfilter/nf_conntrack_l3proto.h>
-#endif
 
 /*
  * Upstream net-next commmit 7e35ec0e8044
